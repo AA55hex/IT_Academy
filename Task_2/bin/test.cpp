21606c8a3e41a163a8008d516d97d550753c7280
AA55hex <roma.gupalo2015@yandex.ru>
2021-01-29 20:21:06 +0300

    updated

--- Task_2/bin/test.cpp
+++ Task_2/bin/test.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <SDL2/SDL_version.h>
-
+#include <SDL_version.h>
 std::ostream& operator<<(std::ostream& ost, const SDL_version& version)
 {
     ost << static_cast<int>(version.major)
         << '.' << static_cast<int>(version.minor)
-        << '.' << static_cast<int>(version.patch) << std::endl;
+        << '.' << static_cast<int>(version.patch)
+        << std::endl;
     return ost;
 }
 
@@ -13,9 +13,12 @@ int main()
 {
     SDL_version version_linked{};
     SDL_version version_compiled{};
+
     SDL_VERSION(&version_compiled);
     SDL_GetVersion(&version_linked);
+
     std::cout << "Compiled:\t" << version_compiled
               << "Linked:\t" << version_linked;
+
     return !std::cout.good();
 }
